d062c3da185bff920a47d5d8eb5ce42a9b52e58c
Dhirendra <djha5051@gmail.com>
2018-05-03 12:42:45 +0530

    first

--- kolibri_pratham_plugin/api.py
+++ kolibri_pratham_plugin/api.py
@@ -1,12 +1,35 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from pip._vendor.html5lib import filters
 from rest_framework import viewsets
 
 from kolibri.auth.api import KolibriAuthPermissionsFilter
 
 from .models import DataStore
 from .serializers import DataStoreSerializer
+from django.http import HttpResponse
+
 
 class DataStoreViewset(viewsets.ModelViewSet):
+    model = DataStore
     serializer_class = DataStoreSerializer
-    filter_backends = (KolibriAuthPermissionsFilter, DjangoFilterBackend)
-    queryset = DataStore.objects.all()
+    filter_backends = (KolibriAuthPermissionsFilter, DjangoFilterBackend, SearchFilter,)
+    queryset = DataStore.objects.all()#raw('SELECT * FROM DataStore where filter_name = %s', [filter_name])
+    filter_fields = ('filter_name', 'table_name')
+    #search_fields = ('^filter_name', '^table_name')
+    #result = DataStore.objects.filter(filter_fields__icontains='filter_name')
+
+    '''
+    def get_queryset(self):
+        queryset = DataStore.objects.all()
+        filter_name = self.request.query_params.get('filter_name', None)
+        table_name = self.request.query_params.get('table_name', None)
+
+        if filter_name:
+            queryset = queryset.filter(filter_name__icontains=filter_name)
+        if table_name:
+            queryset = queryset.filter(table_name=table_name)
+        elif filter_name and table_name:
+            queryset = queryset.filter(filter_name__icontains=filter_name, table_name=table_name)
+
+        return queryset'''
--- kolibri_pratham_plugin/serializers.py
+++ kolibri_pratham_plugin/serializers.py
@@ -4,13 +4,15 @@ from kolibri.auth.models import Facility
 
 from .models import DataStore
 
-class DataStoreSerializer(serializers.ModelSerializer):
 
+class DataStoreSerializer(serializers.ModelSerializer):
     data = serializers.JSONField(default='\{\}')
+    filter_name = serializers.CharField(default='enter filter')
+    table_name = serializers.CharField(default='enter name')
     facility = serializers.PrimaryKeyRelatedField(queryset=Facility.objects.all())
 
     class Meta:
         model = DataStore
         fields = (
-            'id', 'data', 'facility',
+            'id', 'data', 'filter_name', 'table_name', 'facility',
         )
