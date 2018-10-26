2c75802837f6e5a52100de1eac3e40c811ba8b6e
pratham <prathamopenschool@pratham.org>
2018-10-26 16:26:53 +0530

    backup commit

--- kolibri_pratham_plugin/models.py
+++ kolibri_pratham_plugin/models.py
@@ -13,6 +13,7 @@ class DataStore(AbstractFacilityDataModel):
     filter_name = models.CharField(max_length=100, default='enter filter name')
     table_name = models.CharField(max_length=100, default='enter table name')
     facility = ForeignKey(Facility)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
 
     def infer_dataset(self, *args, **kwargs):
         return self.facility.dataset
